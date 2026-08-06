5a81688fff839510a343798e13be4e85af03d843
Anita-1205 <anitamudaliar002@gmail.com>
2026-08-06 20:32:40 +0530

    Refactor event listeners for button navigation

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-document.getElementById("startBtn").addEventListener("click", function () {
+const startBtn = document.getElementById("startBtn");
+const continueBtn = document.getElementById("continueBtn");
+
+startBtn.addEventListener("click", function () {
 
     document.getElementById("page1").classList.add("hidden");
-
     document.getElementById("page2").classList.remove("hidden");
+
+});
+
+continueBtn.addEventListener("click", function () {
+
+    document.getElementById("page2").classList.add("hidden");
+    document.getElementById("page3").classList.remove("hidden");
 
 });
